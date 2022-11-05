e13ffb4ee61ae4c369b89db9b399f846e3a0b269
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-11-05 14:57:22 -0700

    moved
    Grenade can't use dynamic weapon base as its not compatible with dynammo

--- schema/items/weapons/sh_grenade.lua
+++ schema/items/weapons/sh_grenade.lua
@@ -3,7 +3,6 @@ ITEM.description = "A throwable explosive to flush out opponents."
 ITEM.model = "models/weapons/w_grenade.mdl"
 ITEM.class = "weapon_frag"
 ITEM.weaponCategory = "grenade"
-ITEM.isGrenade = true
 ITEM.classes = {CLASS_EOW}
 ITEM.flag = "g"
 ITEM.width = 1
