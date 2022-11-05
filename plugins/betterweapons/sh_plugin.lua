5fbabf5671fac1f75db51c3bd873782b7619b2b0
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-11-05 01:11:26 -0700

    dynammo
    Dynamic ammunition works as advertised, now.
    
    Pressing R with an empty gun will remove a magazine from your inventory and load it into your gun. Pressing R with more than 1 bullet will unload your gun, placing that magazine into your inventory, but will restock your gun with another magazine from your inventory. Pressing R with 1 bullet or less will remove a magazine from your inventory and be fed into your gun to be loaded. Needs stress testing.

--- plugins/betterweapons/sh_plugin.lua
+++ plugins/betterweapons/sh_plugin.lua
@@ -10,16 +10,37 @@ if (SERVER) then
             local weapon = client:GetActiveWeapon()
             local ammoType = game.GetAmmoName(weapon:GetPrimaryAmmoType())
             -- if the ammo isn't null or has no ammo value (hands, melees)
-            if ammoType and weapon:Clip1() ~= -1 and client:GetAmmoCount(ammoType) <= 1 and weapon:Clip1() <= 1 then
-                local items = client:GetCharacter():GetInv():GetItems() -- returns a table of the player's items to go through
-	            -- go through the player's entire inventory,
-	            for k, v in pairs(items) do
-                    if v.ammo == ammoType then
-                        client:SetAmmo(v:GetData("rounds", v.ammoAmount) + client:GetAmmoCount(ammoType), ammoType)
-                        v:Remove()
-                        break
-                    end
-	            end
+            if ammoType and weapon:Clip1() ~= -1 and client:GetAmmoCount(ammoType) == 0 and ammoType != "Buckshot" then
+                -- if the gun has less than or equal to 1 bullet and the total ammo is less than or equal to one, then
+                if weapon:Clip1() <= 1 then
+                    local items = client:GetCharacter():GetInv():GetItems() -- returns a table of the player's items to go through
+	                -- go through the player's entire inventory,
+	                for k, v in pairs(items) do
+                        if v.ammo == ammoType then
+                            client:SetAmmo(v:GetData("rounds", v.ammoAmount) + client:GetAmmoCount(ammoType), ammoType)
+                            v:Remove()
+                            break
+                        end
+	                end
+                    -- if the gun has more than 1 bullet in its clip, then
+                elseif weapon:Clip1() > 1 then
+                    local items = client:GetCharacter():GetInv():GetItems() -- returns a table of the player's items to go through
+	                -- go through the player's entire inventory, check if there is another magazine in their inventory
+	                for k, v in pairs(items) do
+                        if v.ammo == ammoType then
+                            -- if there a magazine in their inventory and it's the same ammo type as the gun,
+                            local mag = weapon:Clip1()
+                            -- set the player's current ammo to the value of the magazine in their inventory,
+                            weapon:SetClip1(0)
+                            client:SetAmmo(v:GetData("rounds", v.ammoAmount) + client:GetAmmoCount(ammoType), ammoType)
+                            -- set that magazine to the player's old ammo count
+                            v:SetData("rounds", mag)
+                            break
+                        end
+                        -- if no magazine is found, do nothing
+	                end
+                    -- continue playing <3
+                end 
             end
         end
     end
