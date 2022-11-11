175f9492bb0ca8c41552ddde0b1345011774cc9b
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-11-11 01:49:14 -0800

    Vitality Attribute
    Vitality not only increases maximum health, it increases the time it takes for you to get up after unconsciousness from damage.
    Added another check function to update the player's maximum health.

--- plugins/bettermedical/sh_plugin.lua
+++ plugins/bettermedical/sh_plugin.lua
@@ -5,9 +5,10 @@ PLUGIN.description = "Medical items that are affected by a medical attribute and
 local defaultPlayerHealth = 100
 
 function Schema:PlayerHurt(client, attacker, health)
-    if health <= 10 and ((client.ragdollCooldown or 0) < CurTime()) then
-        client:SetRagdolled(true, 30)
-        client:GetCharacter():UpdateAttrib("vitality", 1)
+    if health <= 10 then
+        local char = client:GetCharacter()
+        client:SetRagdolled(true, 30 - (char:GetAttribute("vitality") or 0))
+        char:UpdateAttrib("vitality", 1)
         client:ChatNotify("Your vision darkens and causes you to fall out of consciousness.")
     end
 end
@@ -25,6 +26,12 @@ end
 -- upon the attribute being updated, set the player's new max health
 function PLUGIN:CharacterAttributeUpdated(client, self, key, value)
     if key == "vitality" then
+        UpdateMaxHealth(client, client:GetCharacter())
+    end
+end
+
+function Schema:PlayerSpawn(client)
+    if client:GetCharacter() then
         UpdateMaxHealth(client, client:GetCharacter())
     end
 end
