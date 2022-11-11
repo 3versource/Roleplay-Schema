373c477f544b5e82d9697d9ea805db9fc70a4f41
OctraSource <90021750+OctraSource@users.noreply.github.com>
2022-11-10 17:15:28 -0800

    MPF Uniform Setup

--- plugins/dynamicclothes/items/outfit_dynamic/sh_cpmdlstandard.lua
+++ plugins/dynamicclothes/items/outfit_dynamic/sh_cpmdlstandard.lua
@@ -7,6 +7,8 @@ ITEM.playermodelBodygroupChanges = 0
 ITEM.playermodel = "models/police.mdl"
 ITEM.isClothingItem = true
 ITEM.forModel = nil
+ITEM.mpf = "MPF-PTRL."
+ITEM.armor = 50
 
 /*
 	forModel must be one of the following:
@@ -36,3 +38,11 @@ ITEM.forModel = nil
 
 	MPF playermodel layout:
 */
+
+
+function ITEM:PopulateTooltip(tooltip)
+	local panel = tooltip:AddRowAfter("name", "armor")
+	panel:SetBackgroundColor(derma.GetColor("Warning", tooltip))
+	panel:SetText("Armor: " .. (self:GetData("equip") and LocalPlayer():Armor() or self:GetData("armor", self.armor)))
+	panel:SizeToContents()
+end
