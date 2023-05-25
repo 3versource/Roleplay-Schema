5632581475f030591526343e18788d0695e3bd8a
OctraSource <90021750+OctraSource@users.noreply.github.com>
2023-05-24 21:33:00 -0700

    strength rework
    takes 2000 punches to max strength, max strength gives +5 damage to the player's melee

--- plugins/strength/sh_plugin.lua
+++ plugins/strength/sh_plugin.lua
@@ -6,13 +6,13 @@ if (SERVER) then
 	function PLUGIN:GetPlayerPunchDamage(client, damage, context)
 		if (client:GetCharacter()) then
 			-- Add to the total fist damage.
-			context.damage = context.damage + (client:GetCharacter():GetAttribute("str", 0) * .15)
+			context.damage = context.damage + (client:GetCharacter():GetAttribute("str", 0) * .5)
 		end
 	end
 
 	function PLUGIN:PlayerThrowPunch(client, trace)
 		if (client:GetCharacter() and IsValid(trace.Entity)) then
-			client:GetCharacter():UpdateAttrib("str", 0.025)
+			client:GetCharacter():UpdateAttrib("str", 0.005)
 		end
 	end
 end
