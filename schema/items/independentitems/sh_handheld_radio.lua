1b1215e038d3960f60e9b46f329d12dbcc68fd16
OctraSource <90021750+OctraSource@users.noreply.github.com>
2023-06-04 22:15:25 -0700

    flag and class removal

--- schema/items/independentitems/sh_handheld_radio.lua
+++ schema/items/independentitems/sh_handheld_radio.lua
@@ -2,8 +2,6 @@
 ITEM.name = "Handheld Radio"
 ITEM.model = Model("models/ug_imports/radio/w_radio.mdl")
 ITEM.description = "A shiny handheld radio with a frequency tuner.\nIt is currently turned %s%s."
-ITEM.classes = {CLASS_EMP, CLASS_EOW}
-ITEM.flag = "v"
 ITEM.category = "Tools"
 
 -- Inventory drawing
--- schema/items/weapons/sh_grenade.lua
+++ schema/items/weapons/sh_grenade.lua
@@ -3,7 +3,5 @@ ITEM.description = "A throwable explosive to flush out opponents."
 ITEM.model = "models/weapons/w_grenade.mdl"
 ITEM.class = "weapon_frag"
 ITEM.weaponCategory = "grenade"
-ITEM.classes = {CLASS_EOW}
-ITEM.flag = "g"
 ITEM.width = 1
 ITEM.height = 1
